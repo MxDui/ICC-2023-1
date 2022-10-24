45a1d46650fc208d6ec26e4bf9b4a2e014478ede
David Rivera Morales <driveramorales46@gmail.com>
2022-10-24 09:44:26 -0500

    Started second project

---
+++ projects/proyecto2/src/main/java/mx/unam/ciencias/icc/proyecto2/Proyecto2.java
@@ -0,0 +1,30 @@
+package mx.unam.ciencias.icc.proyecto2;
+
+public class Proyecto2 {
+
+    private static final int ERROR_USO = 1;
+
+    private static void uso() {
+        System.out.println("Uso: java -jar proyecto2.jar [opciones] archivo");
+        System.out.println("Opciones:");
+        System.out.println("  -v, --version              Muestra la versión.");
+        System.exit(ERROR_USO);
+    }
+
+    public static void main(String[] args) {
+
+        if (args.length != 1)
+            uso();
+
+        try {
+            // Here execute the program with class Aplicacion
+        } catch (IllegalArgumentException iae) {
+            System.err.println(iae.getMessage());
+            System.exit(ERROR_USO);
+        } catch (Exception e) {
+            System.err.println("Error inesperado.");
+            System.exit(ERROR_USO);
+        }
+
+    }
+}
