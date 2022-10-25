d0b970dc283fed2e5496fa28868fde8159ae5b50
David Rivera Morales <driveramorales46@gmail.com>
2022-10-24 23:20:06 -0500

    Sort and reverse sort working with two and three args but with stdin not

--- projects/proyecto2/src/main/java/mx/unam/ciencias/icc/proyecto2/Aplicacion.java
+++ projects/proyecto2/src/main/java/mx/unam/ciencias/icc/proyecto2/Aplicacion.java
@@ -1,5 +1,16 @@
 package mx.unam.ciencias.icc.proyecto2;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Aplicacion {
 
     private enum Opcion {
@@ -43,20 +54,179 @@ public class Aplicacion {
         }
     }
 
+    public void ordenar() {
+
+    }
+
     public void ordenar(String archivo) {
-        System.out.println("Ordernar" + archivo);
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(archivo), "UTF-8"));
+
+            Lista<Registro> lista = new Lista<Registro>();
+
+            String linea = br.readLine();
+
+            while (linea != null) {
+                lista.agregaFinal(new Registro(linea));
+                linea = br.readLine();
+            }
+
+            br.close();
+
+            lista = lista.mergeSort(
+                    (a, b) -> a.getLinea().codePointAt(0) - b.getLinea().codePointAt(0));
+
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(archivo), "UTF-8"));
+
+            for (Registro r : lista) {
+                bw.write(r.getLinea());
+                bw.newLine();
+            }
+
+            bw.close();
+
+        } catch (IOException ioe) {
+            System.err.println("Error al leer el archivo.");
+            System.exit(1);
+        }
     }
 
     public void ordenar(String archivo, String archivoSalida) {
-        System.out.println("Ordernar \t" + archivo + " a " + archivoSalida);
+
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(archivo), "UTF-8"));
+
+            Lista<Registro> lista = new Lista<Registro>();
+
+            String linea = br.readLine();
+
+            while (linea != null) {
+                lista.agregaFinal(new Registro(linea));
+                linea = br.readLine();
+            }
+
+            br.close();
+
+            lista = lista.mergeSort(
+                    (a, b) -> a.getLinea().codePointAt(0) - b.getLinea().codePointAt(0));
+
+            BufferedWriter bw = new BufferedWriter(
+                    new OutputStreamWriter(new FileOutputStream(archivoSalida), "UTF-8"));
+
+            for (Registro r : lista) {
+                bw.write(r.getLinea());
+                bw.newLine();
+            }
+
+            bw.close();
+
+        } catch (IOException ioe) {
+            System.err.println("Error al leer el archivo.");
+            System.exit(1);
+        }
     }
 
-    public void invertir(String archivo) {
-        System.out.println("Invertir" + archivo);
+    public void invertirOrden() throws IOException {
+        // this shit is not working
+        // try {
+        // Scanner sc = new Scanner(System.in);
+        // sc.useDelimiter("\n");
+        // Lista<Registro> lista = new Lista<Registro>();
+        // String linea = sc.nextLine();
+        // while (linea != null) {
+        // lista.agregaFinal(new Registro(linea));
+        // linea = sc.nextLine();
+        // }
+        // lista = lista.mergeSort(
+        // (a, b) -> a.getLinea().codePointAt(0) - b.getLinea().codePointAt(0));
+
+        // BufferedWriter bw = new BufferedWriter(
+        // new OutputStreamWriter(new FileOutputStream("reversa.txt"), "UTF-8"));
+        // for (Registro r : lista) {
+        // bw.write(r.getLinea());
+        // bw.newLine();
+        // }
+        // bw.close();
+
+        // sc.close();
+
+        // } catch (InputMismatchException ime) {
+        // System.err.println("Error al leer el archivo.");
+        // System.exit(1);
+        // }
+
     }
 
-    public void invertir(String archivo, String archivoSalida) {
-        System.out.println("Invertir \t" + archivo + " a " + archivoSalida);
+    public void invertirOrden(String archivo) {
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(archivo), "UTF-8"));
+
+            Lista<Registro> lista = new Lista<Registro>();
+
+            String linea = br.readLine();
+
+            while (linea != null) {
+                lista.agregaFinal(new Registro(linea));
+                linea = br.readLine();
+            }
+
+            br.close();
+
+            lista = lista.mergeSort(
+                    (a, b) -> a.getLinea().codePointAt(0) - b.getLinea().codePointAt(0));
+
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(archivo), "UTF-8"));
+
+            for (Registro r : lista.reversa()) {
+                bw.write(r.getLinea());
+                bw.newLine();
+            }
+
+            bw.close();
+
+        } catch (IOException ioe) {
+            System.err.println("Error al leer el archivo.");
+            System.exit(1);
+        }
+    }
+
+    public void invertirOrden(String archivo, String archivoSalida) {
+
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(archivo), "UTF-8"));
+
+            Lista<Registro> lista = new Lista<Registro>();
+
+            String linea = br.readLine();
+
+            while (linea != null) {
+                lista.agregaFinal(new Registro(linea));
+                linea = br.readLine();
+            }
+
+            br.close();
+
+            lista = lista.mergeSort(
+                    (a, b) -> a.getLinea().codePointAt(0) - b.getLinea().codePointAt(0));
+
+            BufferedWriter bw = new BufferedWriter(
+                    new OutputStreamWriter(new FileOutputStream(archivoSalida), "UTF-8"));
+
+            for (Registro r : lista.reversa()) {
+                bw.write(r.getLinea());
+                bw.newLine();
+            }
+
+            bw.close();
+
+        } catch (IOException ioe) {
+            System.err.println("Error al leer el archivo.");
+            System.exit(1);
+        }
     }
 
     public void analizarArgs(
@@ -65,13 +235,23 @@ public class Aplicacion {
             Proyecto2.uso();
         }
         if (args.length == 1) {
-            ordenar(args[0]);
+            if (args[0].equals(Opcion.ORDERNAR.getOpcion())) {
+                ordenar();
+            } else if (args[0].equals(Opcion.INVERTIR.getOpcion())) {
+                try {
+                    invertirOrden();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                throw new IllegalArgumentException("Opcion no valida");
+            }
         }
         if (args.length == 2) {
             if (args[0].equals(Opcion.ORDERNAR.getOpcion())) {
                 ordenar(args[1]);
             } else if (args[0].equals(Opcion.INVERTIR.getOpcion())) {
-                invertir(args[1]);
+                invertirOrden(args[1]);
             } else {
                 Proyecto2.uso();
             }
@@ -80,7 +260,7 @@ public class Aplicacion {
             if (args[0].equals(Opcion.ORDERNAR.getOpcion())) {
                 ordenar(args[1], args[2]);
             } else if (args[0].equals(Opcion.INVERTIR.getOpcion())) {
-                invertir(args[1], args[2]);
+                invertirOrden(args[1], args[2]);
             } else {
                 Proyecto2.uso();
             }
